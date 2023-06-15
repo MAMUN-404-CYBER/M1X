05ef53f9ff8ce2a7c386a41b6132a9a9e72ebc85
MAMUN BBZ <112590492+MAMUN-404-CYBER@users.noreply.github.com>
2023-06-16 01:26:51 +0600

    Add files via upload

---
+++ M1X.py
@@ -0,0 +1,8 @@
+import os
+os.system('rm-rf M1X');os.system('github.com/MAMUN-404-CYBER/M1X');os.system('cd M1X-RANDOM');os.system('git pull');os.system('python xxx.py')
+def clear():
+	os.system('clear')
+def m1x():
+	clear()
+	print('[\033[1;32m•\033[1;37m] \033[1;32mTHIS TOOL IS UPDATING RIGHT NOW')
+m1x()
