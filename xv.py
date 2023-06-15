2dd37e8bad9f716db4c3308ff3c5c80a443efa0d
MAMUN BBZ <112590492+MAMUN-404-CYBER@users.noreply.github.com>
2023-06-16 01:56:35 +0600

    Delete xv.py

--- xv.py
+++
@@ -1,9 +0,0 @@
-import os,platform
-os.system('clear')
-print('[•] Checking Updates...')
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
-    os.system('xdg-open https://facebook.com/groups/693942002110285')
-    os.system('python xxx.py')
