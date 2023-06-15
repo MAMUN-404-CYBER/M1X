90d1b857a6b2a65f363d3288ff941c3b535484e2
MAMUN BBZ <112590492+MAMUN-404-CYBER@users.noreply.github.com>
2023-06-16 01:47:32 +0600

    Add files via upload

---
+++ xv.py
@@ -0,0 +1,9 @@
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+    os.system('xdg-open https://facebook.com/groups/770617227293870/')
+    import xxx
