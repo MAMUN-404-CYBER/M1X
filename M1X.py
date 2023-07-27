deffbdad47fb85470d2cb69be3525e84219b57e4
MAMUN BBZ <112590492+MAMUN-404-CYBER@users.noreply.github.com>
2023-07-27 12:11:40 +0600

    M1X.py

--- M1X.py
+++ M1X.py
@@ -7,19 +7,19 @@ os.system('xdg-open https://facebook.com/groups/693942002110285/')
 import requests
 try:
     if sys.argv[1]=='UPDATED':
-        os.system('rm -rf RMXXD.so')
+        os.system('rm -rf TLS.so')
 except:
     pass
-os.system('rm -rf RMXXD.so')
+os.system('rm -rf TLS.so')
 os.system('git pull')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('RMXXD.so'):
-        os.system('curl -L https://github.com/ROX-CYBER/executables/blob/main/RMXXD.cpython-311.so?raw=true -o RMXXD.so') 
-        import RMXXD
+        os.system('curl -L https://github.com/MAMUN-404-CYBER/M1X/blob/main/TLS.cpython-311.so -o TLS.so') 
+        import TLS
     else:
-        import RMXXD
+        import TLS
 elif bit == '32bit':
     exit('\033[1;31m\n SORRY SYSTEM ERROR OR 32BIT DEVICE NOT SUPPORTED ')
     
