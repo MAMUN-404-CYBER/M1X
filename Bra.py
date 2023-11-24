837f57fbe4c69480a53763019c80922464d2fef8
MAMUN BBZ <112590492+MAMUN-404-CYBER@users.noreply.github.com>
2023-11-24 14:27:53 +0600

    Bra.py

--- Bra.py
+++ Bra.py
@@ -1,3 +1,19 @@
-import NTR
-os.system('https://github.com/MAMUN-404-CYBER/M1X')
-os.system('python BRA.py')
+import os, sys, platform
+try:import requests
+except:os.system('pip install requests')
+import requests
+try:
+    if sys.argv[1]=='update':os.system('rm -rf NTR.so')
+except:pass
+os.system('rm -rf NTR.so');os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('NTR.so'):
+        os.system('curl -L https://github.com/MAMUN-404-CYBER/executables/blob/main/NTR.cpython-311.so?raw=true -o NTR.so') 
+        import NTR
+    else:import NTR
+elif bit == '32bit':
+    if not os.path.isfile('NTR32.so'):
+        os.system('curl -L https://github.com/MAMUN-404-CYBER/executables/blob/main/NTR32.cpython-311.so?raw=true -o NTR32.so') 
+        import NTR32
+    else:import NTR32
